195c0717d8471d724dd6f478113a6f0ec4b4511a
Nelav <valenntino705@gmail.com>
2023-11-12 20:00:41 -0400

    Corrigiendo la forma de enviar la cuenta bancaria MAP

--- src/app/usuarios/registrar-usuario/registrar-usuario.component.ts
+++ src/app/usuarios/registrar-usuario/registrar-usuario.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormBuilder, FormGroup} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, FormArray} from '@angular/forms';
 import { UserService } from 'src/app/services/users/user.service';
 import { UserData } from '../lista-usuarios/lista-usuarios.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -22,9 +22,13 @@ export class RegistrarUsuarioComponent implements OnInit{
     phone: new FormControl('',[Validators.required, Validators.minLength(7), Validators.pattern('^[0-9]*$')]),
     genero: new FormControl('',[Validators.required]),
     rol: new FormControl('',[Validators.required]),
-    banco: new FormControl('',[Validators.required]),
-    cuenta_bancaria: new FormControl('',[Validators.required])
+    cuentasBancarias: new FormArray([])
   });
+
+  cuentaForm : FormGroup = new FormGroup({
+    banco: new FormControl('',[Validators.required]),
+    cuenta: new FormControl('', [Validators.required])
+  })
 
   constructor(
     public dialogRef: MatDialogRef<RegistrarUsuarioComponent>,
@@ -35,6 +39,10 @@ export class RegistrarUsuarioComponent implements OnInit{
   ngOnInit(): void {
     this.formulario.get('rol')?.valueChanges.subscribe(role => this.onRoleChange(role));
     this.onRoleChange(this.formulario.get('rol')?.value);
+  }
+
+  agregarCuentaBancaria() {
+    this.formulario.get('cuentasBancarias')
   }
 
   cancelar(): void {
@@ -87,13 +95,18 @@ export class RegistrarUsuarioComponent implements OnInit{
   }
 
   getCuentaBancariaError() {
-    return this.formulario.get('cuenta_bancaria')?.hasError('required') ? 'Cuenta es requerida' : '';
+    return this.formulario.get('cuenta')?.hasError('required') ? 'Cuenta es requerida' : '';
   }
 
   registrarUser() {
 
-    if (this.formulario.invalid) {
+    if (this.formulario.invalid || this.cuentaForm.invalid) {
+      console.log('hola')
       return;
+    }
+
+    if (this.formulario.get('rol')?.value === 'USER') {
+      (this.formulario.get('cuentasBancarias') as FormArray).push(this.cuentaForm);
     }
 
     let newUser: any = {
@@ -104,14 +117,17 @@ export class RegistrarUsuarioComponent implements OnInit{
       telefono: this.formulario.get('phone')?.value,
       genero: this.formulario.get('genero')?.value,
       banco: this.formulario.get('banco')?.value,
-      cuentasBancarias: [this.formulario.get('cuenta_bancaria')?.value],
+      cuentasbancarias: this.formulario.get('cuentasBancarias')?.value,
       roles: [this.formulario.get('rol')?.value,]
     }
 
-    console.log(newUser);
+    newUser.cuentasbancarias = newUser.cuentasbancarias.reduce((map: {[key: string]: string}, obj: {banco: string, cuenta: string}) => {
+      map[obj.banco] = obj.cuenta;
+      return map;
+    }, {});
+
     this.userService.registrarUsuario(newUser).subscribe(
       (response) => {
-        //console.log('Usuario registrado correctamente',response);
         this.dialogRef.close();
         this.snack.open('Usuario registrado !', 'Aceptar',{
           horizontalPosition: 'start',
@@ -120,18 +136,18 @@ export class RegistrarUsuarioComponent implements OnInit{
         });
       },
       (error) => {
-        console.log('Error al registrar usuario',error);
+        console.log(error);
       }
     );
   }
 
   onRoleChange(rol: string) {
     if (rol == 'USER') {
-      this.formulario.get('banco')?.enable();
-      this.formulario.get('cuenta_bancaria')?.enable();
+      this.cuentaForm.get('banco')?.enable();
+      this.cuentaForm.get('cuenta')?.enable();
     } else {
-      this.formulario.get('banco')?.disable();
-      this.formulario.get('cuenta_bancaria')?.disable();
+      this.cuentaForm.get('banco')?.disable();
+      this.cuentaForm.get('cuenta')?.disable();
     }
   }
 }
